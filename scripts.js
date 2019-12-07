1fed31b9fff910404f054aac68435ba68480556d
Phillip Dunham <phillipwwd@gmail.com>
2019-12-06 20:05:31 -0800

    Nothing to see here, Move along.

--- scripts.js
+++ scripts.js
@@ -1,1 +1,14 @@
 // for coding quiz
+var counter = document.getElementsByClassName("timetest");
+var startButton = document.getElementsByClassName("start")
+console.log(startButton);
+
+addEventListener("click", )//working
+// startButton
+
+// counter[0].innerHTML = "Replaces placeholder."; this works, but..
+
+counter[0].textContent = "Replaces placeholder.";
+var interval = setInterval(function(){
+
+},1000);
