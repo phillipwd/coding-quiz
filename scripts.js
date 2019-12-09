eeaac4ecc3f8943adad3feb41613bc1ca1259d05
Phillip Dunham <phillipwwd@gmail.com>
2019-12-08 21:06:33 -0800

    Countdown start timer works.

--- scripts.js
+++ scripts.js
@@ -1,14 +1,39 @@
 // for coding quiz
 var counter = document.getElementsByClassName("timetest");
-var startButton = document.getElementsByClassName("start")
-console.log(startButton);
+var startButton = document.getElementsByClassName("start");
+var startCountDown = document.getElementsByClassName("startCountDown");
+var numHolder = 5;
+var elapsed = 0;
+console.log(startButton);//works
+console.log(startCountDown[0].textContent);
+var start = false;
 
-addEventListener("click", )//working
-// startButton
+startButton[0].addEventListener("click", function(){
+    //alert("Click"); //works
+    start = true;
+    changeNumber();
+    
+})//working
 
-// counter[0].innerHTML = "Replaces placeholder."; this works, but..
-
-counter[0].textContent = "Replaces placeholder.";
-var interval = setInterval(function(){
-
-},1000);
+function changeNumber(){
+    if(start){
+        var interval = setInterval(function(){
+            elapsed++;
+            startCountDown[0].textContent = numHolder - elapsed;
+            console.log(startCountDown[0].textContent);
+            
+            if(startCountDown[0].textContent === "0"){
+                clearInterval(interval);
+                startTest();
+            }
+            console.log(elapsed);
+            
+        },1000);
+    }
+}
+function startTest(){
+    if(start === true || startCountDown[0].textContent === "0"){
+        console.log("start the test!");
+        
+    }
+}
